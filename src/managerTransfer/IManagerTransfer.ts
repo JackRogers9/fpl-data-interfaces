5da7b2f5175cde2cbbf826512c54d96bd372fc7a
Jack Rogers <jack.rogers@iress.com>
2022-08-29 18:29:21 +0100

    chore: Added interface for IManagerTransfer.ts

---
+++ src/managerTransfer/IManagerTransfer.ts
@@ -0,0 +1,9 @@
+export interface IManagerTransfer {
+    elementIn: number;
+    elementInCost: number;
+    elementOut: number;
+    elementOutCost:number;
+    entry: number;
+    event: number;
+    time: string;
+}
